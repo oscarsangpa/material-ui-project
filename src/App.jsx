830699d09a208b49cfd54bad90e9e8770ad0aad0
oscarsangpa <oscarsangrador@gmail.com>
2023-02-06 19:03:18 +0100

    feat: clean files and install material ui

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Container, Grid, Typography } from "@mui/material"
+
+function App() {
+  
+  return (
+   <Container>
+      <header>
+        <Typography align="center" marginY={5} commponent="h1" variant="h3">
+          Searcher news
+        </Typography>
+      </header>
+   </Container>
+   
+  )
+}
+
+export default App
